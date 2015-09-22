05a21404d915b576886a29369724eba710888f96
bfosberry <brendan@codeship.com>
2015-09-21 20:51:12 -0500

    Updating default port, allowing configurable port

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	DefaultPort = 80
+	DefaultPort = "8080"
 )
 
 type Server interface {
@@ -16,7 +16,7 @@ type Server interface {
 }
 
 type server struct {
-	port  int
+	port  string
 	ship  *Ship
 	token string
 }
@@ -27,8 +27,13 @@ func NewServer(ship *Ship) (Server, error) {
 		return nil, errors.New("No Token provided")
 	}
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = DefaultPort
+	}
+
 	return &server{
-		port:  DefaultPort,
+		port:  port,
 		ship:  ship,
 		token: token,
 	}, nil
@@ -38,53 +43,66 @@ func (s *server) Listen() {
 	http.HandleFunc("/_ping", s.ping)
 	http.HandleFunc("/update", s.update)
 	http.HandleFunc("/action", s.action)
-	http.ListenAndServe(fmt.Sprintf(":%d", s.port), nil)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", s.port), nil)
 }
 
 func (s *server) ping(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("received ping")
 	w.WriteHeader(200)
 }
 
 func (s *server) update(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("received update")
 	if r.Header.Get("Authorization") != s.token {
+		fmt.Println("received bad auth token")
 		w.WriteHeader(401)
 		return
 	}
 	sh, err := NewShipFromJson(r.Body)
 	if err != nil {
+		fmt.Printf("failed to parse %s\n", err.Error())
 		w.WriteHeader(500)
 		return
 	}
+	fmt.Printf("%+v\n", sh)
 	s.ship.Update(sh)
 	w.WriteHeader(200)
 }
 
 func (s *server) action(w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("Authorization") != s.token {
+		fmt.Println("received bad auth token")
 		w.WriteHeader(401)
 		return
 	}
 	a, err := NewActionFromJson(r.Body)
 	if err != nil {
+		fmt.Printf("failed to parse %s\n", err.Error())
 		w.WriteHeader(500)
 		return
 	}
 	switch a.Type {
 	case "hit":
+		fmt.Println("got hit")
 		var hp HitPayload
 		hp, err = a.HitPayload()
 		if err != nil {
+			fmt.Printf("failed to to get payload: %s\n", err)
 			s.ship.Hit(hp.Damage, hp.Enemy, a.State)
 		}
 	case "scan":
+		fmt.Println("got scan")
 		var sp ScanPayload
 		sp, err = a.ScanPayload()
 		if err != nil {
+			fmt.Printf("failed to to get payload: %s\n", err)
 			s.ship.Scanned(sp.Enemy)
 			s.ship.Update(a.State)
 		}
 	}
 	if err != nil {
+		fmt.Printf("got error: %s\n", err)
 		w.WriteHeader(500)
 		return
 	}
